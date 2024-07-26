ed8c7755a2209cae65ba1713cf14d8d6a8522f4c
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-07-26 16:14:33 +0500

    Update Lambda_Function_Exercise_Questions.py

--- Lambda_Function_Exercise_Questions.py
+++ Lambda_Function_Exercise_Questions.py
@@ -6,7 +6,7 @@ print(mul(5,7))
 # Q.2/ Question: You have a list of numbers [1, 2, 3, 4, 5]. Use the map function and a lambda function to create a new list where each number is increased by 10.
 
 list1 = [1,2,3,4,5]
-list2 = list(map(lambda x: x + 10,list1))
+list2 = list(map(lambda x:x+10,list1))
 print(list2)
 
 # Q.3/ Question: Given a list of numbers [3, 6, 9, 12, 15, 18, 21], use the filter function to select numbers greater than 10. Then, use the reduce function to calculate the sum of the filtered numbers.
